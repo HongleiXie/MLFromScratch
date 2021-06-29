95de17fff90e1c5b7e1a0342e267cbeb806b4523
HongleiXie <xhonglei2007@gmail.com>
2021-06-29 16:16:03 -0400

    Add files via upload

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,75 +1,74 @@
-# -*- coding: utf-8 -*-
-"""
-Linear regression by gradient descent.
-"""
+import tensorflow as tf
+import random
 
 
-from numpy import *
-import os
+def synthetic_data(w, b, num_examples):
+    """
+    y = Xw + b + noise
+    """
+    X = tf.zeros((num_examples, w.shape[0]))
+    X += tf.random.normal(shape=X.shape)
+    y = tf.matmul(X, tf.reshape(w, (-1, 1))) + b
+    y += tf.random.normal(shape=y.shape, stddev=0.01)
+    y = tf.reshape(y, (-1, 1))
+    return X, y
 
+def linreg(X, w, b):
+    return tf.matmul(X, w) + b
 
-def compute_error(b,m,points):
-	totalError = 0
-	for i in range(0,len(points)):
-		x = points[i,0]
-		y = points[i,1]
-		totalError += (y - (m*x+b))**2
-	return totalError/float(len(points))
+def squared_loss(y_hat, y):
+    # note: we are not computing average here, just the sum
+    return (y_hat - tf.reshape(y, y_hat.shape))**2 / 2 # reshape just in case, sometimes y y_hat could be either (1,n) or (n,1)
 
+def sgd(params, grads, lr, batch_size):
+    """
+    mini-batch SGD
+    """
+    for param, grad in zip(params, grads):
+        param.assign_sub(lr * grad / batch_size) # since we are not computing average in the last step so we devide by batch size here
 
-
-def step_gradient(b_current, m_current, points, learning_rate):
-	b_gradient = 0
-	m_gradient = 0
-	N = float(len(points))
-	for i in range(0, len(points)):
-		x = points[i, 0]
-		y = points[i, 1]
-		b_gradient += -(2/N) * (y - ((m_current * x) + b_current))
-		m_gradient += -(2/N) * x * (y - ((m_current * x) + b_current))
-
-	new_b = b_current - (learning_rate * b_gradient)
-	new_m = m_current - (learning_rate * m_gradient)
-	return [new_b, new_m]
-
-
-# determine when the search finds a solution
-def gradient_descent_runner(points, initial_m, initial_b, learning_rate, max_num_iter, tol = 1e-3):
-	b = initial_b
-	m = initial_m
-	num_iter = 0
-	new_error = 0
-	old_error = compute_error(initial_b, initial_m, points)
-	tot_error = 999
-	while (tot_error > tol and num_iter < max_num_iter):
-		num_iter = num_iter + 1
-		new_b, new_m = step_gradient(b, m, array(points), learning_rate)
-		old_error = compute_error(b, m, points)
-		new_error = compute_error(new_b, new_m, points)
-		tot_error = abs(new_error - old_error)
-		b = new_b
-		m = new_m
-	return [new_b, new_m, num_iter]
-
-
-def run():
-	os.chdir('/some_dir/')
-	points = np.genfromtxt('data.csv', delimiter=',')
-
-	learning_rate = 0.0001
-	# y = mx + b
-	initial_b = 0
-	initial_m = 0
-	num_iterations = 1000
-	max_num_iter = 1000
-
-	print ("Starting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m, compute_error(initial_b, initial_m, points)))
-	print ("Running...")
-
-	[final_b, final_m, num_iter] = gradient_descent_runner(points, initial_m, initial_b, learning_rate, num_iterations)
-	print ("After {0} iterations b = {1}, m = {2}, error = {3}".format(num_iter, final_b, final_m, compute_error(final_b, final_m, points)))
-
+def data_iter(batch_size, features, labels):
+    """
+    generate a data iter which takes the input (batch, features, labels)
+    output: mini batch of (X,y)
+    """
+    num_examples = len(features)
+    indices = list(range(num_examples))
+    random.shuffle(indices)
+    for i in range(0, num_examples, batch_size):
+        j = tf.constant(indices[i:min(i + batch_size, num_examples)]) # handle the last batch whose size may be smaller than batch_size
+        yield tf.gather(features, j), tf.gather(labels, j)
 
 
 if __name__ == '__main__':
-	run()
+
+    # generate synthetic data
+    true_w = tf.constant([2, -3.4])
+    true_b = 4.2
+    features, labels = synthetic_data(true_w, true_b, 1000)
+
+    # init
+    w = tf.Variable(tf.random.normal(shape=(2, 1), mean=0, stddev=0.01), trainable=True)
+    b = tf.Variable(tf.zeros(1), trainable=True)
+
+    # start to train the linear regression model
+    lr = 0.03 # learning rate
+    num_epochs = 3
+    batch_size = 10
+
+
+    for epoch in range(num_epochs):
+        for X, y in data_iter(batch_size, features, labels):
+            with tf.GradientTape() as g:
+                l = squared_loss(linreg(X, w, b), y)  # mini-batch loss of `X`和`y`
+            # compute the gradients of l w.r.t [`w`, `b`]
+            dw, db = g.gradient(l, [w, b])
+            # update parameters
+            sgd([w, b], [dw, db], lr, batch_size) # not entirely correct cuz the last batch has size <= batch_size, but ignore the nuance for now
+
+        train_l = squared_loss(linreg(features, w, b), labels)
+        print(f'epoch {epoch + 1}, loss {float(tf.reduce_mean(train_l)):f}')
+
+    # print out the distance between the estimated parameters and the true parameters
+    print(f'diff in w: {true_w - tf.reshape(w, true_w.shape)}')
+    print(f'diff in b: {true_b - b}')
